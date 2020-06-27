b1d63a9c6888cb3e4aa81ef41fbb0deb16727502
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2020-06-27 16:05:37 -0300

    alterando o modal

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,19 @@ for (let card of cards) {
     card.addEventListener('click', function() {
         const imageId = card.getAttribute('id');
         modalOverlay.classList.add('active');
-        modalOverlay.querySelector('iframe').src = `assets/${imageId}`;
+
+        /*modalOverlay.querySelector('iframe').src = `assets/${imageId}`;
+        modalOverlay.querySelector("#p-description").innerHTML = ${description};*/
+        
+        const tituloReceitas = card.querySelector('#h3-do-card_content').innerText;
+        const autorReceitas = card.querySelector('#p-do-card_info').innerText;
+        
+        modalOverlay.querySelector('img').src = `assets/${imageId}`;
+
+        /* modalOverlay.querySelector(".modal-img").innerHTML = `${imageId}`;*/
+
+        modalOverlay.querySelector('h3').innerText = `${tituloReceitas}`;
+        modalOverlay.querySelector('p').innerText = `${autorReceitas}`;
 
 
     });
@@ -13,5 +25,9 @@ for (let card of cards) {
 
 document.querySelector('.close-modal').addEventListener('click', function() {
     modalOverlay.classList.remove('active');
-    modalOverlay.querySelector('iframe').src = "";
+    modalOverlay.querySelector('img').src = "";
+
+   /*
+    modalOverlay.querySelector("h3").innerText = ""
+    modalOverlay.querySelector("p").innerText = "" */
 });
